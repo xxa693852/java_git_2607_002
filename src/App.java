cffdc24779e1cefbcdd663915534462988019f9e
xxa693852 <xxa693852@gmail.com>
2026-07-17 05:25:21 +0800

    dev cherry pick main

--- src/App.java
+++ src/App.java
@@ -2,7 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        System.out.println("main-update-#1-#2");
+        System.out.println("main-update-#1-#2-#3");
+
+        System.out.println("admin-update-#1-#2");
 
         System.out.println("dev-update-#1");
     }
